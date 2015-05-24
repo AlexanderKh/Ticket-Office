db32bec36c3223595e4263b08c2394a38f35201e
Alexander Khomchenko <alexanderkh@interlink-ua.com>
2015-05-24 12:15:39 +0200

    Creating Objects

--- src/main/java/alex/App.java
+++ src/main/java/alex/App.java
@@ -4,10 +4,9 @@ package alex;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ){
+        System.out.println(12);
         System.out.println( "Hello World!" );
     }
 }
